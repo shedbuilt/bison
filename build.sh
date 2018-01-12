c5683200ae0246c916cf3a3a501d641c45a9ea4a
Auston Stewart <auston@shedbuilt.net>
2018-01-11 21:43:08 -0500

    Adding a toolchain build mode.

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-./configure --prefix=/usr \
-            --docdir=/usr/share/doc/bison-3.0.4
-make -j $SHED_NUMJOBS
-make "DESTDIR=${SHED_FAKEROOT}" install
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ./configure --prefix=/tools || return 1
+        ;;
+    *)
+        ./configure --prefix=/usr \
+                    --docdir=/usr/share/doc/bison-3.0.4 || return 1
+        ;;
+esac
+make -j $SHED_NUMJOBS || return 1
+make DESTDIR="$SHED_FAKEROOT" install || return 1
