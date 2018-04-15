cf6627126871d106223c74f82c0842e9eec6cf79
Auston Stewart <auston@shedbuilt.net>
2018-04-15 11:10:58 -1000

    Updating build script for blank

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 case "$SHED_BUILD_MODE" in
     toolchain)
-        ./configure --prefix=/tools || return 1
+        ./configure --prefix=/tools || exit 1
         ;;
     *)
         ./configure --prefix=/usr \
-                    --docdir=/usr/share/doc/bison-3.0.4 || return 1
+                    --docdir=/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION} || exit 1
         ;;
 esac
-make -j $SHED_NUM_JOBS || return 1
-make DESTDIR="$SHED_FAKE_ROOT" install || return 1
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install
