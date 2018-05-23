71b8f1de915e3aa1680c2ce912ce3fe5e57922b2
Auston Stewart <auston@shedbuilt.net>
2018-05-22 17:00:21 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        ./configure --prefix=/tools || exit 1
-        ;;
-    *)
-        ./configure --prefix=/usr \
-                    --docdir=/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION} || exit 1
-        ;;
-esac
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_PREFIX='/usr'
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    SHED_PKG_LOCAL_PREFIX='/tools'
+fi
+SHED_PKG_LOCAL_DOCDIR="${SHED_PKG_LOCAL_PREFIX}/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+
+# Configure
+./configure --prefix="$SHED_PKG_LOCAL_PREFIX" \
+            --docdir="$SHED_PKG_LOCAL_DOCDIR" &&
+
+# Build & Install
 make -j $SHED_NUM_JOBS &&
-make DESTDIR="$SHED_FAKE_ROOT" install
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+
+# Prune Documentation
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+fi
